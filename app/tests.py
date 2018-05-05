057683c38044545bc3f59a9b71668f9198251976
Konstantin Selyunin <selyunin.k.v@gmail.com>
2018-05-05 16:11:55 +0200

    adding failing test

--- app/tests.py
+++ app/tests.py
@@ -8,7 +8,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_home_page(self):
         home_page = self.app.get('/')
-        assert home_page.data.decode('utf-8') == 'Hello, World! This is version: 0.0.1'
+        assert home_page.data.decode('utf-8') == 'Hello, World! This is version: 1.0.1'
 
 if __name__ == '__main__':
     unittest.main()
